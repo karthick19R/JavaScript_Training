2b0e52ad27131a8a9c834a6beb15701c37e9bd01
karthickr <karthickr@ngpwebsmart.com>
2025-12-22 12:13:24 +0530

    tried closure

--- closure.js
+++ closure.js
@@ -1,12 +1,28 @@
-function counter(){
-    let count =0;
-    function innercounter(){
-        count++;
-        console.log(count)
-    }
-    return innercounter;
+// function counter(){
+//     let count =0;
+//     function innercounter(){
+//         count++;
+//         console.log(count)
+//     }
+//     return innercounter;
+// }
+
+// const incrementCounter = counter();
+// incrementCounter()
+// incrementCounter()
+
+function createAdvancedCounter() {
+  let count = 0;
+
+  return {
+    increment: () => ++count,
+    decrement: () => --count,
+    reset: () => { count = 0; return count; }
+  };
 }
 
-const incrementCounter = counter();
-incrementCounter()
-incrementCounter()
+const counter = createAdvancedCounter();
+console.log(counter.increment()); 
+console.log(counter.increment()); 
+console.log(counter.decrement()); 
+console.log(counter.reset());     
